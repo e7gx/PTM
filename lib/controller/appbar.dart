a44e2f34fe56b87f902699d6f3d905a129f5dd2f
e7gx <abalgabo@gmail.com>
2023-12-29 14:58:28 +0300

    .

---
+++ lib/controller/appbar.dart
@@ -0,0 +1,35 @@
+// import 'package:flutter/material.dart';
+// import 'package:first_time/controller/home_page.dart';
+
+// class NavigationDrawer extends StatelessWidget {
+//   const NavigationDrawer({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return const Drawer(
+//       child: SingleChildScrollView(
+//         child: Column(
+//           crossAxisAlignment: CrossAxisAlignment.stretch,
+//           children: <Widget>[
+//             buildHeader(context),
+//             buildMenuItems(context),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+
+//   Widget buildHeader(BuildContext context) => Container(
+//         padding: EdgeInsets.only(top: MediaQuery.of(context).padding.top),
+//       );
+
+//   Widget buildMenuItems(BuildContext context) => Column(
+//         children: [
+//           ListTile(
+//             leading: const Icon(Icons.menu),
+//             title: const Text('ho'),
+//             onTap: () {},
+//           ),
+//         ],
+//       );
+// }
